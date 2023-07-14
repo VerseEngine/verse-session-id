4a75eee8e5ca3ab5473afd9e373f985c80e6b56a
hosoi-appland <hosoi+verse@appland.co.jp>
2023-07-15 06:30:59 +0900

    Bump version to  1.0.1 (documentation)

--- README.md
+++ README.md
@@ -5,7 +5,9 @@
 [<img alt="MIT" src="https://img.shields.io/github/license/VerseEngine/verse-session-id?style=for-the-badge" height="20">](https://github.com/VerseEngine/verse-session-id/blob/main/LICENSE)
 [<img alt="MIT" src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=for-the-badge" height="20">](https://github.com/VerseEngine/verse-three/pulls)
 
-Session ID for [@VerseEngine/verse-core](https://github.com/VerseEngine/verse-session-id)
+
+ID with signature/verification functions.  
+Used as session ID in [@VerseEngine/verse-core](https://github.com/VerseEngine/verse-session-id).
 
 
 ## Usage
@@ -23,5 +25,24 @@ pub fn verify_string(session_id: &str, signature: &str, data: &str) -> bool {
     };
 
     sid.verify(vec![data.as_bytes()], &ss).is_ok()
+}
+```
+
+### Generate ID
+```rust
+let id_pair = new_session_id_pair()?;
+let session_id = id_pair.get_id();
+// to string
+let s = format!("{}", session_id);
+```
+
+
+### Create a signature
+```rust
+pub fn sign_string(&self, data: &str) -> Result<String> {
+    let id_pair = ...;
+    Ok(id_pair
+        .sign(vec![data.as_bytes()])?
+        .to_string())
 }
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 //! # verse-session-id
 //!
 //! [<img alt="crates.io" src="https://img.shields.io/crates/v/verse-session-id.svg?style=for-the-badge&color=fc8d62&logo=rust" height="20">](https://crates.io/crates/verse-session-id)
+//! [<img alt="docs.rs" src="https://img.shields.io/docsrs/verse-session-id?style=for-the-badge&logo=docs.rs" height="20">](https://docs.rs/verse-session-id)
+//! [<img alt="MIT" src="https://img.shields.io/github/license/VerseEngine/verse-session-id?style=for-the-badge" height="20">](https://github.com/VerseEngine/verse-session-id/blob/main/LICENSE)
+//! [<img alt="MIT" src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=for-the-badge" height="20">](https://github.com/VerseEngine/verse-three/pulls)
 //!
-//! Session ID for [@VerseEngine/verse-core](https://github.com/VerseEngine/verse-core)
+//! ID with signature/verification functions.  
+//! Used as session ID in [@VerseEngine/verse-core](https://github.com/VerseEngine/verse-session-id).
 //!
 //! ## Usage
 //! ### Signature Verification
@@ -19,7 +23,28 @@
 //!   };
 //!
 //!   sid.verify(vec![data.as_bytes()], &ss).is_ok()
-//!}
+//! }
+//! ```
+//!
+//!
+//! ### Generate ID
+//! ```rust
+//! let id_pair = new_session_id_pair()?;
+//! let session_id = id_pair.get_id();
+//! // to string
+//! let s = format!("{}", session_id);
+//! ```
+//!
+//!
+//! ### Create a signature
+//! ```rust
+//! pub fn sign_string(&self, data: &str) -> Result<String> {
+//!     let id_pair = ...;
+//!     Ok(id_pair
+//!         .sign(vec![data.as_bytes()])?
+//!         .to_string())
+//! }
+//! ```
 mod session_id;
 pub use session_id::*;
 
